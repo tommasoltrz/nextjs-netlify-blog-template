f34498ad101e7830a1750a4dca8657617848bd99
Tommaso Laterza <tommasoltrz@gmail.com>
2021-02-15 10:03:49 +0100

    fix wrong typingi

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,9 +4,8 @@ type Config = {
   readonly base_url: string;
   readonly site_title: string;
   readonly site_description: string;
-  readonly site_keywords: { keyword: string }[];
+  readonly site_keywords: string[];
   readonly posts_per_page: number;
-  readonly twitter_account: string;
   readonly github_account: string;
 };
 
