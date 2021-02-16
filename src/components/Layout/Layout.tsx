f1890f4d81ed25332dbd18888cd91958d4b40368
Tommaso Laterza <tommasoltrz@gmail.com>
2021-02-16 17:39:58 +0100

    fixing config

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Navigation from "../Navigation";
-import styles from "./Layout.module.scss";
+// import styles from "./Layout.module.scss";
 
 type Props = {
   children: React.ReactNode;
 };
 export default function Layout({ children }: Props) {
   return (
-    <div className={styles.root}>
+    <div>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
