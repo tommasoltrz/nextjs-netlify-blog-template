478ae4bff0a2b7e21ca9a56ac3fda436373b0721
Tommaso Laterza <tommasoltrz@gmail.com>
2021-02-16 17:37:42 +0100

    fixing config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     locales: ["en", "fr", "it"],
     defaultLocale: "en",
   },
+  pageExtensions: ["mdx", "tsx"],
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
       ...[
